7534f858216df09c769729b78ea4a46026876a28
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-03 23:14:17 +0600

    practice retrieving all rows, single row, and list of column values

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller {
@@ -28,4 +29,23 @@ class DemoController extends Controller {
     public function delete( Request $request ) {
         return Brand::where( 'id', '=', $request->id )->delete();
     }
+
+    // Retrieving All Rows
+    function getAll() {
+        // return Brand::get();
+        return Brand::all();
+    }
+
+    // Retrieving Single Row
+    public function singleRow() {
+        return Brand::first();
+        // return Brand::find( 2 );
+    }
+
+    // Retrieving List of Column Values
+    public function columnList() {
+        // return Product::pluck( 'price' );
+        return Product::pluck( 'price', 'title' );
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,7 @@ Route::post( '/create-brand', [DemoController::class, 'create'] );
 Route::post( '/update-brand/{id}', [DemoController::class, 'update'] );
 Route::post( '/create-update/{brandName}', [DemoController::class, 'updateOrCreate'] );
 Route::get( '/delete-brand/{id}', [DemoController::class, 'delete'] );
+
+Route::get( '/get-all', [DemoController::class, 'getAll'] );
+Route::get( '/single-row', [DemoController::class, 'singleRow'] );
+Route::get( '/column-list', [DemoController::class, 'columnList'] );
